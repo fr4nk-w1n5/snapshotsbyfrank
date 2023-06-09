507b80c3d11d552b65ed917cafa48e2d6939ea49
Frank <frank.obed25@gmail.com>
2023-06-08 18:22:13 -0700

    adding picture for header

--- main.js
+++ main.js
@@ -3,6 +3,6 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').
     classList.toggle
-    ('window-scrolled'), window.
-    scrollY > 0
+    ('window-scrolled', window.
+    scrollY > 0);
 })
