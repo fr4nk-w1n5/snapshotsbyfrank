099491c428a440d67b56e69bd32845f5fdd5d281
Frank <frank.obed25@gmail.com>
2023-06-12 23:57:26 -0700

    almost done with header section

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ window.addEventListener('scroll', () => {
     ('window-scrolled', window.
     scrollY > 0);
 })
+
+// CONTACT BUTTONS (circular text buttons) //
+
+const textButtons = document.querySelectorAll('.contact_btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector("p");
+
+    text.innerHTML = text.innerHTML.split('').map
+    ((character, index) => `<span style="transform: 
+    rotate(${index * 12}
+    deg)">${character}</span>`).join('')
+})
